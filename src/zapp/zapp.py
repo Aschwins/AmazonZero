01f361f8a17f67350007129f7ad6e40068b0ce45
Aschwins <aschwin.schilperoort@gmail.com>
2021-05-26 22:31:21 +0200

    updated zapp

--- src/zapp/zapp.py
+++ src/zapp/zapp.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, json
+from amazonzero.test import hello_world
 app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return json.dumps('hello')
+    a = hello_world()
+    return json.dumps(a)
 if __name__ == '__main__':    
     # listen on all IPs 
     app.run(host='0.0.0.0')
