e3e491ed84dae2ee6d32dc79fe53fa52d8c21057
Aschwins <aschwin.schilperoort@gmail.com>
2021-05-26 22:24:25 +0200

    renamed app

--- src/zapp/zapp.py
+++ src/zapp/zapp.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, json
 app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
-def index():    
-    # Parse request body for model input
-    return json.dumps("hello")
+def index():
+    return json.dumps('hello')
 if __name__ == '__main__':    
     # listen on all IPs 
     app.run(host='0.0.0.0')
