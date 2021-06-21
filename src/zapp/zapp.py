a63bfe09d78b4580a024f28ff63f225de3f533b9
Aschwins <aschwin.schilperoort@gmail.com>
2021-06-21 17:48:04 +0200

    added better response

--- src/zapp/zapp.py
+++ src/zapp/zapp.py
@@ -1,10 +1,53 @@
-from flask import Flask, request, json
+from flask import Flask, request, jsonify
 from amazonzero.test import hello_world
+
+import pickle
+import boto3
+import numpy as np
+from io import BytesIO
+from amazonzero.board import Board
+from amazonzero.player import ForestPlayer, Predictor, Actor
+
 app = Flask(__name__)
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     a = hello_world()
-    return json.dumps(a)
+    return jsonify(a)
+
+@app.route('/move', methods=['POST'])
+def move():
+    boardstate = request.json.get('boardstate')
+    if not boardstate:
+        return "Request must contain json with 'boardstate'", 403
+
+    # Create a board
+    boardstate = np.array(boardstate)
+    width = len(boardstate)
+    n_amazons = sum(sum(np.array(boardstate) == 1))
+    b = Board(width, n_amazons)
+    b.matrix = boardstate
+
+    # Load the model
+    s3 = boto3.resource('s3')
+    with BytesIO() as data:
+        s3.Bucket("game-of-amazons").download_fileobj("models/rfg3_3000pg.pkl", data)
+        data.seek(0)    # move back to the beginning after writing
+        model = pickle.load(data)
+
+    # Create the player      
+    predictor = Predictor(model, max_mem=1_000)
+    actor = Actor()
+    fp = ForestPlayer(predictor, actor)
+
+    next_boardstate = fp.move(b)
+
+    return_data = {
+        "next_boardstate": next_boardstate.tolist()
+    }
+
+    return jsonify(return_data)
+
 if __name__ == '__main__':    
     # listen on all IPs 
     app.run(host='0.0.0.0')
